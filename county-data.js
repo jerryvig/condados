6ad85e58d6e3d084b2c4e80997411e78c1b71b75
Vigil, Jerry (Contractor) <Jerry_Vigil@comcast.com>
2018-01-11 09:44:12 -0800

    First commit of the county data project.

---
+++ county-data.js
@@ -0,0 +1,48 @@
+
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+
+const processCounty = (county_name, state) => {
+  const url = 'http://www.city-data.com/county/' + county_name.replace(/ /g, '_') + '_County-' + state + '.html';
+  fetch(url)
+    .then((res) => {
+      return res.text();
+    }).then((body) => {
+
+      const $ = cheerio.load(body);
+      const fb = $('#foreign-born-population');
+      let fbLines = fb.text().split('\n');
+
+      for (let line of fbLines) {
+        if (line.startsWith('Number of foreign born residents')) {
+          let afterColon = line.split(':')[1].trim();
+          console.log(afterColon.split(' ')[0]);
+        }
+        if (line.startsWith(county_name + ' County:')) {
+          let afterColon = line.split(':')[1];
+          let pct = afterColon.split('%')[0];
+          console.log(pct + '%');
+        }
+      }
+  }).then(() => {
+    doNextCounty();
+  });
+};
+
+
+var counties = [
+  {county_name: 'Broward', state: 'FL'},
+  {county_name: 'Fort Bend', state: 'TX'},
+  {county_name: 'Santa Clara', state: 'CA'},
+];
+
+const doNextCounty = () => {
+  var nextCounty = counties.shift();
+  if (nextCounty) {
+    setTimeout(() => {
+      processCounty(nextCounty.county_name, nextCounty.state);
+    }, 500);
+  }
+};
+
+doNextCounty();
