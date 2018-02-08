e5165b51f1477aa29e0c1730533c7e7fdcea570e
Vigil, Jerry (Contractor) <Jerry_Vigil@comcast.com>
2018-02-07 18:58:41 -0800

    Adding WA data.

--- county-data.js
+++ county-data.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 const fs = require('fs');
 const json2csv = require('json2csv');
 
-const state = 'OR';
+const state = 'WA';
 const INPUT_FILE_NAME = `county_list_${state}.csv`;
 const OUTPUT_FILE_NAME = `county_data_${state}.json`;
 const CSV_OUTPUT = `county_data_${state}.csv`;
