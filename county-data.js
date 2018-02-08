617f5cf0c25d15a1618173a051be05815b6b4a22
Vigil, Jerry (Contractor) <Jerry_Vigil@comcast.com>
2018-02-08 14:38:09 -0800

    fdjals.

--- county-data.js
+++ county-data.js
@@ -4,11 +4,11 @@ const fetch = require('node-fetch');
 const fs = require('fs');
 const json2csv = require('json2csv');
 
-const state = 'WA';
+const state = 'MS';
 const INPUT_FILE_NAME = `county_list_${state}.csv`;
 const OUTPUT_FILE_NAME = `county_data_${state}.json`;
 const CSV_OUTPUT = `county_data_${state}.csv`;
-const THROTTLE_PERIOD = 1200;
+const THROTTLE_PERIOD = 1250;
 
 const processCounty = (county_name, state) => {
   const url = 'http://www.city-data.com/county/' + county_name.replace(/ /g, '_') + '_County-' + state + '.html';
